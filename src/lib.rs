61758b4ec03a5a6b885b284720bd1ce5c0fefb0d
kleedodo <kleedodo@duck.com>
2025-03-13 16:37:01 +0800

    在有key的情况下尾行要加换行

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ pub mod gemini;
 pub mod siliconflow;
 
 pub async fn save_to_file(keys: &[&str], filename: &str) -> anyhow::Result<()> {
-    let all_key = keys.join("\n");
+    let mut all_key = keys.join("\n");
+    if !all_key.is_empty() {
+        all_key.push('\n');
+    }
     tokio::fs::write(filename, all_key.as_bytes()).await?;
     Ok(())
 }
