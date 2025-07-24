64e9414aa4fe870135f6e9922cecababc44ca265
kleedodo <kleedodo@duck.com>
2025-07-24 15:01:44 +0800

    gemini test

--- src/gemini.rs
+++ src/gemini.rs
@@ -2,7 +2,7 @@ use anyhow::Ok;
 use reqwest::{Client, Response};
 
 pub async fn say_hi(key: &str, client: Client) -> anyhow::Result<Response> {
-    let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-8b:generateContent?key={key}");
+    let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key={key}");
     let resp = client
         .post(url)
         .json(&serde_json::json!({
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use clap::ValueEnum;
 use indicatif::ProgressBar;
 use reqwest::{Response, StatusCode};
@@ -15,11 +17,16 @@ pub enum KeyType {
     Gemini,
     Ppinfra,
 }
-pub async fn save_to_file(keys: &[&str], filename: &str) -> anyhow::Result<()> {
+pub async fn save_to_file<I, S>(keys: I, filename: &str) -> anyhow::Result<()>
+where
+    I: Deref<Target = [S]>,
+    S: AsRef<str>,
+{
     if keys.is_empty() {
         return Ok(());
     }
-    let mut all_key = keys.join("\n");
+    let all_key = keys.iter().map(|k| k.as_ref()).collect::<Vec<&str>>();
+    let mut all_key = all_key.join("\n");
     all_key.push('\n');
     tokio::fs::write(filename, all_key.as_bytes()).await?;
     Ok(())
@@ -34,10 +41,11 @@ pub async fn check_resp(
     let mut total_pro = 0_f64;
     let mut have_banlance = Vec::new();
     let mut no_balance = Vec::new();
-    let mut unknow = Vec::new();
+    let mut forbidden = Vec::new();
     let mut results = Vec::new();
     let mut pro_banlace = Vec::new();
     let mut disable_keys = Vec::new();
+    let mut status_key = Vec::new();
     for resp in responses.into_iter() {
         let (key, resp) = resp;
         match channel_type {
@@ -62,8 +70,9 @@ pub async fn check_resp(
                     results.push(format!("{key}, {total_balance}, {charge_balance}"));
                 }
                 Err(err) => {
-                    unknow.push(key);
-                    tracing::debug!("`{key}`: `{err}`")
+                    forbidden.push(key);
+                    tracing::debug!("`{key}`: `{err}`");
+                    status_key.push(format!("{key}, {err:?}"));
                 }
             },
             KeyType::Deepseek => match deepseek::total_balance(resp).await {
@@ -77,29 +86,37 @@ pub async fn check_resp(
                     results.push(format!("{key}, {balance}"));
                 }
                 Err(err) => {
-                    unknow.push(key);
-                    tracing::debug!("`{key}`: `{err}`")
+                    forbidden.push(key);
+                    tracing::debug!("`{key}`: `{err}`");
+                    status_key.push(format!("{key}, {err:?}"));
                 }
             },
             KeyType::Gemini => match resp {
                 Ok(resp) => {
-                    match resp.status() {
+                    let status = resp.status();
+                    match status {
                         StatusCode::OK => {
                             have_banlance.push(key);
                         }
                         StatusCode::TOO_MANY_REQUESTS => {
                             no_balance.push(key);
                         }
+                        StatusCode::BAD_REQUEST | StatusCode::FORBIDDEN => {
+                            forbidden.push(key);
+                        }
                         _ => {
-                            unknow.push(key);
+                            status_key.push(format!("{key}, {status}"));
                         }
                     }
                     let text = resp.text().await;
-                    tracing::debug!("`{key}`, `{:?}`", text);
+                    tracing::debug!("`{key}`, `{text:?}`");
+                    results.push(format!("{key}, {status}, {text:?}"));
                 }
                 Err(err) => {
-                    unknow.push(key);
-                    tracing::debug!("`{key}`: `{err}`")
+                    forbidden.push(key);
+                    tracing::debug!("`{key}`: `{err}`");
+                    results.push(format!("{key}, {err:?}"));
+                    status_key.push(format!("{key}, {err:?}"));
                 }
             },
             KeyType::Ppinfra => match ppinfra::total_balance(resp).await {
@@ -113,31 +130,37 @@ pub async fn check_resp(
                     results.push(format!("{key}, {balance}"));
                 }
                 Err(err) => {
-                    unknow.push(key);
-                    tracing::debug!("`{key}`: `{err}`")
+                    forbidden.push(key);
+                    tracing::debug!("`{key}`: `{err}`");
+                    status_key.push(format!("{key}, {err:?}"));
                 }
             },
         }
     }
 
-    save_to_file(&have_banlance, "keys").await?;
-    save_to_file(&no_balance, "no_balance_keys").await?;
-    save_to_file(&pro_banlace, "pro_keys").await?;
-    save_to_file(&unknow, "401_keys").await?;
-    save_to_file(&disable_keys, "disable_keys").await?;
+    save_to_file(have_banlance, "keys").await?;
+    save_to_file(no_balance, "no_balance_keys").await?;
+    save_to_file(pro_banlace, "pro_keys").await?;
+    save_to_file(forbidden, "401_keys").await?;
+    save_to_file(disable_keys, "disable_keys").await?;
+    save_to_file(results.clone(), "results").await?;
+    save_to_file(status_key, "status_key").await?;
     bar.finish();
     tracing::info!("详细：+++++++++++++++++++++++++++++++++++++++++++++");
-    for key in results {
-        println!("{key}");
-    }
     match channel_type {
         KeyType::Siliconflow => {
+            for key in results {
+                println!("{key}");
+            }
             println!("total: {total}, total pro: {total_pro}");
         }
         KeyType::Deepseek | KeyType::Ppinfra => {
+            for key in results {
+                println!("{key}");
+            }
             println!("total: {total}");
         }
-        _ => {}
+        KeyType::Gemini => {}
     }
     Ok(())
 }
